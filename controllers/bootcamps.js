d58109331373dcb2e47618ea61ce9246f8d68f1c
voja <voja.milenkovic@nfq.asia>
2020-04-22 22:54:13 +0200

    Mongoose Error Handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,7 +10,7 @@ exports.getBootcamps = async (req, res, next) => {
     
     res.status(200).json({ success: true, data: bootcamps, count: bootcamps.length })
   } catch (error) {
-    res.status(400).json({ success: false })
+    next(err)
   }
 }
 
@@ -22,12 +22,12 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id)
 
     if (!bootcamp) {
-      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`), 404)
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
 
     res.status(200).json({ success: true, data: bootcamp })
-  } catch (error) {
-    next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`), 404)
+  } catch (error) {``
+    next(error)
   }
 }
 
@@ -43,7 +43,7 @@ exports.createBootcamp = async (req, res, next) => {
       data: bootcamp,
     }) 
   } catch (error) {
-    res.status(400).json({ success: false })
+    next(err)
   }
 }
 
@@ -58,12 +58,12 @@ exports.updateBootcamp = async (req, res, next) => {
     })
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false })
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     } 
 
     res.status(200).json({ success: true, data: bootcamp }) 
   } catch (error) {
-    return res.status(400).json({ success: false })
+    next(err)
   }
 }
 
@@ -75,12 +75,12 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false })
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
 
     res.status(200).json({ success: true, data: {} }) 
   } catch (error) {
-    return res.status(400).json({ success: false })
+    next(err)
   }
 }
 
