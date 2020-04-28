ae57e1a8fc68013044508bbf7e4cad299d229037
voja <voja.milenkovic@nfq.asia>
2020-04-28 09:35:44 +0200

    Courses CRUD

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const {
   getCourses,
+  getCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse,
 } = require('../controllers/courses')
 
 // mergeParams to get re-routing to work (coming from /:bootcampId/courses)
@@ -9,6 +13,13 @@ const router = express.Router({ mergeParams: true })
 router
   .route('/')
   .get(getCourses)
+  .post(createCourse)
+
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(updateCourse)
+  .delete(deleteCourse)
 
 module.exports = router
 
